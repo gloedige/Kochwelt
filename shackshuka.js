072bd3dc415c2424a99e408f28a38a22cfb37c1e
FelixJemlich <felix.jemlich@gmail.com>
2025-06-24 10:44:11 +0200

    Button Style and JS overworked

--- shackshuka.js
+++ shackshuka.js
@@ -37,7 +37,4 @@ function calculate(event) {
 var x = document.getElementById("quantity").value;
 document.getElementById("amount").innerHTML = x;
 console.log(x);
-var a = "150g"
-let b = a*
->>>>>>> b0f6218 (Add first JS to html)
 }
