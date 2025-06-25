405a526b3015213d3b4f6eeeb91af83a74448a31
gloedige <gerd.loedige@posteo.de>
2025-06-25 13:04:42 +0200

    create calulation and return to html table for first incredient

--- carbonara.js
+++ carbonara.js
@@ -6,10 +6,19 @@
     function calcQuantity(){
         let currentValue = factorInput.value;
         console.log("Aktueller Wert im Input-Feld: ", currentValue);
-
+        
         factorInput.value = currentValue;
+
+        let varCalcQuantity = factorInput.value*quantityOfOne;
+        cell1.innerHTML = `${varCalcQuantity}g Pasta, lang und dünn`;
     }
-    // updatePortionFactor();
+    let table = document.getElementById('table-incredients');
+    let row = table.insertRow(0);
+    let cell1 = row.insertCell(0);
+
+    let quantityOfOne = 120;
+    let varCalcQuantity = factorInput.value*quantityOfOne;
+    cell1.innerHTML = `${varCalcQuantity}g Pasta, lang und dünn`;
 
     formPortion.addEventListener('submit', function(event){
         event.preventDefault();
