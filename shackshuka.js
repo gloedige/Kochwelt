b9942ab37d8b207a23851ddf246afae08d03a663
FelixJemlich <felix.jemlich@gmail.com>
2025-06-25 15:22:43 +0200

    Overworking recipe calc

--- shackshuka.js
+++ shackshuka.js
@@ -12,38 +12,11 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
 modal.style.display = "none";
 }
-<<<<<<< HEAD
-//recipe calc// 
-let IngridientAmount1 = 150,
-    IngridientAmount2 = 1,
-    IngridientAmount3 = 1,
-    IngridientAmount4 = 0.25,
-    IngridientAmount5 = 0.25,
-    IngridientAmount6 = 1,
-    IngridientAmount7 = 0.75;
-function calculate(event) {
-    event.preventDefault(); 
-    let portionen = Number(document.getElementById("portionen").value);
-    document.getElementById("IngridientAmount1").innerHTML = (IngridientAmount1 * portionen)+" g";
-    document.getElementById("IngridientAmount2").innerHTML = IngridientAmount2 * portionen;
-    document.getElementById("IngridientAmount3").innerHTML = IngridientAmount3 * portionen;
-    document.getElementById("IngridientAmount4").innerHTML = (IngridientAmount4 * portionen)+ " ,gestr.";
-    document.getElementById("IngridientAmount5").innerHTML = (IngridientAmount5 * portionen)+ " Prise(n)";
-    document.getElementById("IngridientAmount6").innerHTML = IngridientAmount6 * portionen;
-    document.getElementById("IngridientAmount7").innerHTML = IngridientAmount7 * portionen;
-=======
-//calc funktion für Rezept//
-function calculate(event) {
+//recipe calc//
+function myFunction(){
     event.preventDefault();
-    const portionen = parseFloat (document.getElementById("portionen").value)
-    document.querySelectorAll(".IngridientAmount").forEach(td => {
-        const basis = parseFloat(td.dataset.basis);
-        console.log(basis);
-        const berechnet = (basis * portionen).toFixed(2).replace(/\.00$/, "");
-        const einheit = td.dataset.info || "";
-        td.textContent = einheit ? `${berechnet} ${einheit}` : berechnet;
-        console.log(berechnet)
-    });
+    var x = document.getElementById('portionen').valueAsNumber;
+    var cells = document.querySelectorAll('IngridientAmount')
+    console.log(td)
 }
-
 
