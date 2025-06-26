fb9fec5a2e480a4d13ee9cc2334926183b253a5a
FelixJemlich <felix.jemlich@gmail.com>
2025-06-26 22:47:47 +0200

    Adjust responsible

--- shackshuka.js
+++ shackshuka.js
@@ -12,5 +12,22 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
 modal.style.display = "none";
 }
-//recipe calc//
-
+//recipe calc// 
+let IngridientAmount1 = 150,
+    IngridientAmount2 = 1,
+    IngridientAmount3 = 1,
+    IngridientAmount4 = 0.25,
+    IngridientAmount5 = 0.25,
+    IngridientAmount6 = 1,
+    IngridientAmount7 = 0.75;
+function calculate(event) {
+    event.preventDefault(); 
+    let portionen = Number(document.getElementById("portionen").value);
+    document.getElementById("IngridientAmount1").innerHTML = (IngridientAmount1 * portionen)+" g";
+    document.getElementById("IngridientAmount2").innerHTML = IngridientAmount2 * portionen;
+    document.getElementById("IngridientAmount3").innerHTML = IngridientAmount3 * portionen;
+    document.getElementById("IngridientAmount4").innerHTML = (IngridientAmount4 * portionen)+ " ,gestr.";
+    document.getElementById("IngridientAmount5").innerHTML = (IngridientAmount5 * portionen)+ " Prise(n)";
+    document.getElementById("IngridientAmount6").innerHTML = IngridientAmount6 * portionen;
+    document.getElementById("IngridientAmount7").innerHTML = IngridientAmount7 * portionen;
+}
