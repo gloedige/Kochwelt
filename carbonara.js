58559f39fc70c7f815343baa3a15c6a54a13f662
FelixJemlich <felix.jemlich@gmail.com>
2025-07-10 11:42:19 +0200

    delete comments correct semantic
     Co-authored-by: Gerd Lödige gloedige@users.noreply.github.com
    
    Co-authored-by: TuncCu TuncCuDev@users.noreply.github.com

--- carbonara.js
+++ carbonara.js
@@ -1,52 +1,47 @@
-// document.addEventListener('DOMContentLoaded', function(){
-    const factorInput = document.getElementById('factor_portion');
-    const formPortion = document.getElementById('formPortion');
-    console.log(factorInput.value);
+const factorInput = document.getElementById('factor_portion');
+const formPortion = document.getElementById('formPortion');
+let table = document.getElementById('table-incredients');
+const amount = [120, 50, 1, 20, 20, 5, 2];
+const unit = [" g "," g "," ", " g "," g "," ml "," g "];
+const incredient = ["Pasta, lang und dünn",
+    "Pancetta, Guanciale",
+    "Ei(er)",
+    "Parmesan, frisch geriebgen",
+    "Pecorino romano, frisch gerieben",
+    "Olivenöl",
+    "Salz und Pfeffer"];
 
-    function calcQuantity(){
-        let currentValue = factorInput.value;
-        console.log("Aktueller Wert im Input-Feld: ", currentValue);
-        
-        factorInput.value = currentValue;
-
-        for (let i=0; i<amount.length; i++){
-            let row = table.rows[i];
-            // console.log("aktuelle Reihe ist: ", row);
-            let cell1 = row.cells[0];
-            // console.log("aktuelle Zelle ist: ", cell1);
-            let quantityOfOne = amount[i];
-            let varUnit = unit[i];
-            let varIncredient = incredient[i];
-            let varCalcQuantity = factorInput.value*quantityOfOne;
-            cell1.innerHTML = `${varCalcQuantity}${varUnit}${varIncredient}`;
-        }
-    }
-    let table = document.getElementById('table-incredients');
-    
-    const amount = [120, 50, 1, 20, 20, 5, 2];
-    const incredient = ["Pasta, lang und dünn",
-        "Pancetta, Guanciale",
-        "Ei(er)",
-        "Parmesan, frisch geriebgen",
-        "Pecorino romano, frisch gerieben",
-        "Olivenöl",
-        "Salz und Pfeffer"
-    ]
-    const unit = [" g "," g "," ", " g "," g "," ml "," g "]
-
+function initTable(){
     for (let i=0; i<amount.length; i++){
         let row = table.insertRow(i);
-        let cell1 = row.insertCell(0);
+        let cell = row.insertCell(0);
         let quantityOfOne = amount[i];
         let varUnit = unit[i];
         let varIncredient = incredient[i];
         let varCalcQuantity = factorInput.value*quantityOfOne;
-        cell1.innerHTML = `${varCalcQuantity}${varUnit}${varIncredient}`;
+        cell.innerHTML = `${varCalcQuantity}${varUnit}${varIncredient}`;
     }
-    
+};
 
-    formPortion.addEventListener('submit', function(event){
-        event.preventDefault();
-        calcQuantity();
-    });
-// });
+function calcQuantity(){
+    let currentValue = factorInput.value;
+    
+    factorInput.value = currentValue;
+
+    for (let i=0; i<amount.length; i++){
+        let row = table.rows[i];
+        let cell = row.cells[0];
+        let quantityOfOne = amount[i];
+        let varUnit = unit[i];
+        let varIncredient = incredient[i];
+        let varCalcQuantity = factorInput.value*quantityOfOne;
+        cell.innerHTML = `${varCalcQuantity}${varUnit}${varIncredient}`;
+    }
+};
+
+initTable();
+
+formPortion.addEventListener('submit', function(event){
+    event.preventDefault();
+    calcQuantity();
+});
