1b61dc6f2cc3c5a22a224a9fe9d3ba070b7efc44
FelixJemlich <felix.jemlich@gmail.com>
2025-07-10 11:45:26 +0200

    delete text variable, delete insert, change position from function.
    Co-authored-by: Gerd Lödige gloedige@users.noreply.github.com
    
    Co-authored-by: TuncCu TuncCuDev@users.noreply.github.com

--- croissant.js
+++ croissant.js
@@ -1,41 +1,19 @@
-    const factorInput = document.getElementById('factor_portion');
-    const formPortion = document.getElementById('formPortion');
-    console.log(factorInput.value);
+const factorInput = document.getElementById('factor_portion');
+const formPortion = document.getElementById('formPortion');
+let table = document.getElementById('table-incredients');
+let amount = [250, 30, 50, 0.5, 12, 115, 5, 125];
+let unit = [" g "," g "," g ", " TL "," g "," g "," g "," "," g "];
+let incredient = [
+    "Mehl",
+    "Zucker",
+    "Butter, weiche",
+    "Salz",
+    "Frischhefe",
+    "Wasser, kaltes",
+    "Milch",
+    "Butter, kalte"];
 
-    function calcQuantity(){
-        let currentValue = factorInput.value;
-        console.log("Aktueller Wert im Input-Feld: ", currentValue);
-        
-        factorInput.value = currentValue;
-
-        for (let i=0; i<amount.length; i++){
-            let row = table.rows[i];
-            // console.log("aktuelle Reihe ist: ", row);
-            let cell1 = row.cells[0];
-            // console.log("aktuelle Zelle ist: ", cell1);
-            let quantityOfOne = amount[i];
-            let varUnit = unit[i];
-            let varIncredient = incredient[i];
-            let varCalcQuantity = factorInput.value*quantityOfOne;
-            cell1.innerHTML = `${varCalcQuantity}${varUnit}${varIncredient}`;
-        }
-        
-    }
-    let table = document.getElementById('table-incredients');
-    
-    let amount = [250, 30, 50, 0.5, 12, 115, 5, 125];
-    let incredient = [
-        "Mehl",
-        "Zucker",
-        "Butter, weiche",
-        "Salz",
-        "Frischhefe",
-        "Wasser, kaltes",
-        "Milch",
-        "Butter, kalte",
-    ]
-    let unit = [" g "," g "," g ", " TL "," g "," g "," g "," "," g "]
-
+function initTable(){
     for (let i=0; i<amount.length; i++){
         let row = table.insertRow(i);
         let cell1 = row.insertCell(0);
@@ -44,11 +22,28 @@
         let varIncredient = incredient[i];
         let varCalcQuantity = factorInput.value*quantityOfOne;
         cell1.innerHTML = `${varCalcQuantity}${varUnit}${varIncredient}`;
-    }
-    
+    };
+};
 
-    formPortion.addEventListener('submit', function(event){
-        event.preventDefault();
-        calcQuantity();
-    });
-    Text()
+function calcQuantity(){
+    let currentValue = factorInput.value;
+    
+    factorInput.value = currentValue;
+
+    for (let i=0; i<amount.length; i++){
+        let row = table.rows[i];
+        let cell1 = row.cells[0];
+        let quantityOfOne = amount[i];
+        let varUnit = unit[i];
+        let varIncredient = incredient[i];
+        let varCalcQuantity = factorInput.value*quantityOfOne;
+        cell1.innerHTML = `${varCalcQuantity}${varUnit}${varIncredient}`;
+    };
+    
+};
+initTable();
+
+formPortion.addEventListener('submit', function(event){
+    event.preventDefault();
+    calcQuantity();
+});
