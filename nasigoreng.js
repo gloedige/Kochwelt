4f75761cf8dadb5cbd8b1c0f53ad3819cbffc22c
FelixJemlich <felix.jemlich@gmail.com>
2025-07-13 12:38:08 +0200

    korrektur defaultwerte nasigoreng.

--- nasigoreng.js
+++ nasigoreng.js
@@ -19,12 +19,12 @@ function calculate(event) {
     document.getElementById("datamenge3").innerHTML = (datamenge3 * inputPortion)+ " EL" + " Erdnussöl" ;
     document.getElementById("datamenge4").innerHTML = (datamenge4 * inputPortion)+ " Zwiebel";
     document.getElementById("datamenge5").innerHTML = (datamenge5 * inputPortion)+ " Knoblauchzehe"; 
-    document.getElementById("datamenge6").innerHTML = (datamenge6 * inputPortion)+ " Chilisote";
+    document.getElementById("datamenge6").innerHTML = (datamenge6 * inputPortion)+ " Chilischote";
     document.getElementById("datamenge7").innerHTML = (datamenge7 * inputPortion)+ " g" + " Ingwer";
     document.getElementById("datamenge8").innerHTML = (datamenge8 * inputPortion)+ " g" + " Möhren";
     document.getElementById("datamenge9").innerHTML = (datamenge9 * inputPortion)+ " Lauchzwiebel";
     document.getElementById("datamenge10").innerHTML = (datamenge10 * inputPortion)+ " Spargelbohnen oder Prinzess-Bohnen";
     document.getElementById("datamenge11").innerHTML = (datamenge11 * inputPortion)+ " Hähnchenbrustfilet";
     document.getElementById("datamenge12").innerHTML = (datamenge12 * inputPortion)+ " EL" + " Sojasauce";
-    document.getElementById("datamenge13").innerHTML = (datamenge13 * inputPortion)+ " EL" + " Ketchup Mani<s";
+    document.getElementById("datamenge13").innerHTML = (datamenge13 * inputPortion)+ " EL" + " Ketchup Manis";
 }
